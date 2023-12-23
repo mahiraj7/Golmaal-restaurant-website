14b050684a91cf7f5285e96e5af409d7d6aa4a16
MAHIRAJ SINGH BAGHELA <126379542+mahiraj7@users.noreply.github.com>
2023-12-23 13:23:24 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}", "menu.html",
+                "./*{html,jss}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
